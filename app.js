daa613f90788c03ceb9e09c626d38cf8fde57c9c
belutoranzo <belen.toranzo@gmail.com>
2023-09-05 17:11:51 -0300

    modifico  app.js con rutas GET

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
-process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 const path = require('path');
 
-app(express.static(public));
+app.use(express.static('public'));
 
 app.get('/404', (req, res) => {
     res.send("Error página no encontrada");
@@ -13,4 +13,33 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 });
+
+app.get('/404', (req, res) => {
+    res.send("Error página no encontrada");
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+});
+
+app.get('/404', (req, res) => {
+    res.send("Error página no encontrada");
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/products.html'));
+});
+});
+
+app.get('/404', (req, res) => {
+    res.send("Error página no encontrada");
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+});
+
+app.get('/404', (req, res) => {
+    res.send("Error página no encontrada");
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/shop.html'));
+});
+});
+
 app.listen(PORT, ()=>console.log(`Servidor escuchando en puerto ${PORT}`));
